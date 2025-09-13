c5965e3b9bf7d89431a9daa272bce5e0bf0cac64
Adaías Magdiel <adaiasmagdiell@gmail.com>
2025-09-13 10:52:36 -0300

    refactor lexer: Updated Tokenize to handle string tokens
    - Moved extractNumber, extractIdentifier, extractComment to helper
    - Moved helper functions for whitespace and identifier checks to helper
    - Added prev() helper method for internal use
    
    BREAKING CHANGE: The lexer no longer produces tokens for numbers, identifiers, comments, or whitespace.
    Only string tokens are emitted.

--- internal/lexer/lexer.go
+++ internal/lexer/lexer.go
@@ -1,8 +1,6 @@
 package lexer
 
 import (
-	"fmt"
-	"strconv"
 	"strings"
 
 	"github.com/adaiasmagdiel/beremiz-go/internal/err"
@@ -47,9 +45,13 @@ func (l *Lexer) peek() byte {
 	return l.content[l.pos]
 }
 
-// func (l *Lexer) next() byte {
-// 	return l.content[l.pos+1]
-// }
+func (l *Lexer) prev() byte {
+	if l.pos-1 >= 0 {
+		return l.content[l.pos-1]
+	} else {
+		return 0
+	}
+}
 
 func (l *Lexer) consume() byte {
 	ch := l.peek()
@@ -63,155 +65,6 @@ func (l *Lexer) consume() byte {
 	}
 
 	return ch
-}
-
-func (l *Lexer) extractNumber() tokens.Token {
-	var literal string = ""
-	var isInt bool = true
-
-	line := l.line
-	col := l.col
-
-	var start int = l.pos
-	for {
-		if l.isAtEnd() {
-			break
-		}
-
-		ch := l.peek()
-		if !l.isNum(ch) {
-			if ch == '.' {
-				if isInt {
-					isInt = false
-				} else {
-					err.LexerError(l.lines, l.getLoc(), "more than one decimal point in number.", -1)
-					l.errorHandler()
-				}
-			} else {
-				break
-			}
-		}
-		l.consume()
-	}
-
-	literal = l.content[start:l.pos]
-	if isInt {
-		n, e := strconv.ParseInt(literal, 10, 64)
-		if e != nil {
-			n = 0.0
-			err.Error(fmt.Sprintf("Unable to convert literal '%s' to int64.", literal))
-			l.errorHandler()
-		}
-		return tokens.Token{
-			Type:    tokens.Int,
-			Literal: n,
-			Loc:     tokens.Loc{File: l.file, Line: line, Col: col},
-		}
-	} else {
-		n, e := strconv.ParseFloat(literal, 64)
-		if e != nil {
-			n = 0.0
-			err.Error(fmt.Sprintf("Unable to convert literal '%s' to float64.", literal))
-			l.errorHandler()
-		}
-		return tokens.Token{
-			Type:    tokens.Float,
-			Literal: n,
-			Loc:     tokens.Loc{File: l.file, Line: line, Col: col},
-		}
-	}
-}
-
-func (l *Lexer) extractIdentifier() tokens.Token {
-	var literal string = ""
-
-	line := l.line
-	col := l.col
-
-	var start int = l.pos
-	for {
-		if l.isAtEnd() {
-			break
-		}
-
-		ch := l.peek()
-		if !l.isValidIdentifier(ch) {
-			break
-		}
-
-		l.consume()
-	}
-
-	literal = l.content[start:l.pos]
-	kind := tokens.Identifier
-
-	if tokens.IsKeyword(literal) {
-		kind = tokens.Keywords[literal]
-	}
-
-	return tokens.Token{
-		Type:    kind,
-		Literal: literal,
-		Loc:     tokens.Loc{File: l.file, Line: line, Col: col},
-	}
-}
-
-func (l *Lexer) extractComment() tokens.Token {
-	l.consume() // Remove #
-
-	isMultiline := false
-	if l.peek() == '[' {
-		isMultiline = true
-	}
-
-	for {
-		if l.isAtEnd() {
-			break
-		}
-
-		ch := l.peek()
-
-		if ch == '\n' {
-			if !isMultiline {
-				break
-			}
-		}
-
-		if ch == '#' {
-			if isMultiline {
-				l.consume()
-				break
-			}
-		}
-
-		l.consume()
-	}
-
-	return tokens.Token{}
-}
-
-func (l *Lexer) isWhitespace(ch byte) bool {
-	switch ch {
-	case ' ', '\t', '\n', '\r', '\f', '\v':
-		return true
-	}
-	return false
-}
-
-func (l *Lexer) isNum(ch byte) bool {
-	return ch >= '0' && ch <= '9'
-}
-
-func (l *Lexer) isAlpha(ch byte) bool {
-	return ch >= 'a' && ch <= 'z' || ch >= 'A' && ch <= 'Z'
-}
-
-func (l *Lexer) isAlphaNum(ch byte) bool {
-	return l.isAlpha(ch) || l.isNum(ch)
-}
-
-func (l *Lexer) isValidIdentifier(ch byte) bool {
-	return l.isAlphaNum(ch) || ch == '_'
 }
 
 func (l *Lexer) isAtEnd() bool {
@@ -242,6 +95,9 @@ func (l *Lexer) Tokenize() []tokens.Token {
 			ts = append(ts, token)
 		} else if ch == '#' {
 			l.extractComment()
+		} else if ch == '\'' || ch == '"' {
+			token := l.extractString()
+			ts = append(ts, token)
 		} else if l.isWhitespace(ch) {
 			l.consume()
 		} else {
